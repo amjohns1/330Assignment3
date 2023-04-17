7ad2bc5cb8e6ee6cc9d62ce443edd3348bcf471b
Erik Overberg <eoverberg3461@gmail.com>
2023-04-16 22:55:46 -0500

    finished again

--- AdamandErik330Assignment3.py
+++ AdamandErik330Assignment3.py
@@ -1,8 +1,15 @@
 from typing import List
 import numpy as np
 
+"""
+Class: CS 330
+Authors: Adam Johnson & Erik Overberg
+Program: Assignment 3
+Implements A* Algorithm and outputs results to a file
+"""
 
-class Graph:
+
+class Graph: #Holds all nodes and connections
     # nodes = None
     # connections = None
 
@@ -14,7 +21,7 @@ class Graph:
         return [c for c in self.connections if c.fromNode == node.nodeNumber]
 
 
-class Node:
+class Node: #duh
     def __init__(self, number, status, costSoFar, estH, estT, previous, x, y):
         self.nodeNumber = number
         self.status = status
@@ -33,7 +40,7 @@ class Node:
         return hash((self.x, self.y))
 
 
-class Connection:
+class Connection: #also duh
     def __init__(self, number, fromNode, toNode, cost):
         self.connectionNumber = number
         self.fromNode = fromNode
@@ -49,97 +56,9 @@ def is_goal(node, goal):
     return node == goal
 
 
-def heuristic(node1, node2):
+def heuristic(node1, node2): #uses manhattan distance
     return abs(node1.x - node2.x) + (node1.y - node2.y)
 
-
-
-
-# def pathfindAStar(graph, start: Node, goal: Node):
-#     # Initialize the record for the start node.
-    # class NodeRecord:
-    #     def __init__(self, node: Node, connection, costSoFar: float, estimatedTotalCost: float):
-    #         self.node = node
-    #         self.connection = connection
-    #         self.costSoFar = costSoFar
-    #         self.estimatedTotalCost = estimatedTotalCost
-#     startRecord = NodeRecord(start, None, 0, heuristic(start, goal))
-#
-#     # Initialize the open and closed lists.
-#     open = [startRecord]
-#     closed = []
-#
-#     # Iterate through processing each node.
-#     while len(open) > 0:
-#         # Find the smallest element in the open list (using the estimatedTotalCost).
-#         current = min(open, key=lambda x: x.estimatedTotalCost)
-#
-#         # If it is the goal node, then terminate.
-#         if current.node == goal:
-#             break
-#
-#         # Otherwise get its outgoing connections.
-#         connections = graph.getConnections(current.node)
-#
-#         # Loop through each connection in turn.
-#         for connection in connections:
-#             # Get the cost estimate for the end node.
-#             endNode = connection.getToNode()
-#             endNodeCost = current.costSoFar + connection.getCost()
-#
-#             # If the node is closed we may have to skip, or remove it from the closed list.
-#             if endNode in [record.node for record in closed]:
-#                 # Here we find the record in the closed list corresponding to the endNode.
-#                 endNodeRecord = next((record for record in closed if record.node == endNode), None)
-#
-#                 # If we didn't find a shorter route, skip.
-#                 if endNodeRecord.costSoFar <= endNodeCost:
-#                     continue
-#
-#                 # Otherwise remove it from the closed list.
-#                 closed.remove(endNodeRecord)
-#
-#                 # We can use the node's old cost values to calculate its heuristic without calling the possibly
-#                 # expensive heuristic function.
-#                 endNodeHeuristic = endNodeRecord.estimatedTotalCost - endNodeRecord.costSoFar
-#
-#             # Skip if the node is open and we've not found a better route.
-#             elif endNode in [record.node for record in open]:
-#                 # Here we find the record in the open list corresponding to the endNode.
-#                 endNodeRecord = next((record for record in open if record.node == endNode), None)
-#
-#                 # If our route is no better, then skip.
-#                 if endNodeRecord.costSoFar <= endNodeCost:
-#                     continue
-#
-#                 # Again, we can calculate its heuristic.
-#                 endNodeHeuristic = endNodeRecord.estimatedTotalCost - endNodeRecord.costSoFar
-#
-#             # Otherwise we know we've got an unvisited node, so make a record for it.
-#             else:
-#                 endNodeRecord = NodeRecord(endNode, None, -1, -1)
-#
-#                 # We'll need to calculate the heuristic value using the function, since we don't have an existing record to use.
-#                 endNodeHeuristic = heuristic(start, endNode)
-#
-#             # We're here if we need to update the node. Update the cost, estimate and connection.
-#             endNodeRecord.costSoFar = endNodeCost
-#             endNodeRecord.connection = connection
-#             endNodeRecord.estimatedTotalCost = endNodeCost + endNodeHeuristic
-#
-#             # And add it to the open list.
-#             if endNode not in [record.node for record in open]:
-#                 open.append(endNodeRecord)
-#         open.remove(current)
-#         closed.append(current)
-#         if current.node != goal:
-#             return list()
-#         else:
-#             path = []
-#         while current.node != start:
-#             path.append(current.connection)
-#             current = current.connection.getFromNode()
-#         return list(reversed(path))
 
 def pathfindAStar(graph, start, goal):
     # This structure is used to keep track of the information we need for each node.
@@ -172,7 +91,7 @@ def pathfindAStar(graph, start, goal):
         # Loop through each connection in turn.
         for connection in connections:
             # Get the cost estimate for the end node.
-            endNode = graph.nodes[connection.toNode-1]
+            endNode = graph.nodes[connection.toNode - 1]
             endNodeCost = currentRecord.costSoFar + connection.cost
 
             # If the node is closed we may have to skip, or remove it from the closed list.
@@ -261,7 +180,6 @@ def main():
     path3 = pathfindAStar(graph, graph.nodes[10], graph.nodes[0])
     path4 = pathfindAStar(graph, graph.nodes[32], graph.nodes[65])
     path5 = pathfindAStar(graph, graph.nodes[57], graph.nodes[42])
-
 
     with open('output.txt', 'w') as f:
         f.write('Nodes:\n')
